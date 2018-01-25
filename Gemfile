716fedf8bf27f096cf47f88a90e9f90bc3751b02
Robert Mosolgo <rdmosolgo@gmail.com>
2018-01-25 11:23:31 -0500

    Add webhook logging

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "thin"
+
+# Added at 2018-01-25 11:15:59 -0500 by rmosolgo:
+gem "sinatra-contrib", "~> 2.0"
--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 require "thin"
+require "sinatra/reloader" if development?
+
+$webhooks = []
 
 get "/" do
-  "Hello world"
+  erb :home
+end
+
+post "/github/webhook" do
+  $webhooks << request.body
+  "OK"
 end
